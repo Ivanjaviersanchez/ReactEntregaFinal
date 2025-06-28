0f5ff695b79362aa1e0c2fff09f6d6a750f9af38
Ivanjaviersanchez <ivanjaviersanchez0108@gmail.com>
2025-06-28 00:51:31 -0300

    EntregaFinal-ajustes-vercel 28/6

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,3 +1,4 @@
+import cartIcon from "../assets/icon-1001596_640.png";
 import { useCart } from "./CartContext.jsx";
 import { Link } from "react-router-dom";
 import "./CartWidget.css";
@@ -8,7 +9,7 @@ function CartWidget({ onClick }) {
 
   return (
     <div className="CartWidgetStyles" onClick={onClick}>
-      <img className="CartStyles" src="/src/assets/icon-1001596_640.png" alt="Carrito" />
+      <img className="CartStyles" src={cartIcon} alt="Carrito" />
       {totalItems > 0 && <span className="Badge">{totalItems}</span>}
     </div>
   );
