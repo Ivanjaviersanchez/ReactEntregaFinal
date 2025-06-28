9c146575abea6946e4935ebc5dc016d190eed98f
Ivanjaviersanchez <ivanjaviersanchez0108@gmail.com>
2025-06-28 00:23:47 -0300

    EntregaFinal-CartWidget 28/6

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,18 +1,17 @@
 import { useCart } from "./CartContext";
 import { Link } from "react-router-dom";
-import cartIcon from "../assets/icon-1001596_640.png";
 import "./CartWidget.css";
 
 function CartWidget({ onClick }) {
+
   const { totalItems } = useCart();
 
   return (
     <div className="CartWidgetStyles" onClick={onClick}>
-      <img className="CartStyles" src={cartIcon} alt="Carrito" />
+      <img className="CartStyles" src="/src/assets/icon-1001596_640.png" alt="Carrito" />
       {totalItems > 0 && <span className="Badge">{totalItems}</span>}
     </div>
   );
 }
 
 export default CartWidget;
-
