67503c1f5423b54e10426aba9c83a0c020340f4b
Ivanjaviersanchez <ivanjaviersanchez0108@gmail.com>
2025-06-29 13:31:52 -0300

    EntregaFinal-home-carrusel 29/6

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Nosotros() {
   return (
     <div>
-      <h2>Sobre nosotros</h2>
+      <h1>Soluciones informáticas</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium laboriosam corporis iste inventore et veniam magnam tempora magni labore temporibus, corrupti explicabo neque harum aspernatur laborum voluptas repellat omnis distinctio?</p>
     </div>
   )
